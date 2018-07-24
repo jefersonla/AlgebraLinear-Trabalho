246f4cf9db3471a7e22e08cbb99ea7b084af8852
jefersonla <oi_je@hotmail.com>
2018-07-24 12:26:08 -0300

    Adicionada documentação provisória

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['strlista',['strLista',['../structstr_lista.html',1,'']]],
+  ['strmatriz',['strMatriz',['../structstr_matriz.html',1,'']]],
+  ['strnolista',['strNoLista',['../structstr_no_lista.html',1,'']]],
+  ['strvetor',['strVetor',['../structstr_vetor.html',1,'']]]
+];
